94aeae2d12fbd4bae149a8afffe3acf681dfddfd
9fdash9 <161096342+9fdash9@users.noreply.github.com>
2026-04-21 17:10:05 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
